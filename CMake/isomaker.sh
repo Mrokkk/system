65ef386cc036f2dfd285552ca09840e65a9df989
Maciek <mrokk@protonmail.com>
2016-07-21 19:01:31 +0200

    Update CMakeLists.txt and isomaker.sh

--- CMake/isomaker.sh
+++ CMake/isomaker.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-if [ ! -f $1/iso/boot/grub/stage2_eltorito ]; then
-    echo Downloading bootloader stage2...
-    wget -q ftp://ftp.free.org/mirrors/rsync.frugalware.org/frugalware-testing/boot/grub/stage2_eltorito
-    mv stage2_eltorito $1/iso/boot/grub/stage2_eltorito
+mkdir -p iso/boot/grub
+
+if [ ! -f iso/boot/grub/menu.lst ]; then
+    echo default 0 > iso/boot/grub/menu.lst
+    echo timeout 0 >> iso/boot/grub/menu.lst
+    echo title My kernel >> iso/boot/grub/menu.lst
+    echo kernel /kernel >> iso/boot/grub/menu.lst
+    # echo module /symbols >>  iso/boot/grub/menu.lst
+    echo boot >> iso/boot/grub/menu.lst
 fi
 
-mkdir -p $1/iso/boot/grub
-cp $1/bin/system $1/iso/kernel
-# cp $1/bin/kernel.sym iso/symbols
-mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o $1/os.iso $1/iso 2> /dev/null
+if [ ! -f iso/boot/grub/stage2_eltorito ]; then
+    echo Downloading bootloader stage2...
+    wget -q ftp://ftp.free.org/mirrors/rsync.frugalware.org/frugalware-testing/boot/grub/stage2_eltorito
+    mv stage2_eltorito iso/boot/grub/stage2_eltorito
+fi
+
+cp system iso/kernel
+# cp bin/kernel.sym iso/symbols
+mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o os.iso iso 2> /dev/null
