eb42264b4d03625dab332830a32d3769aefa9476
Maciek <mrokk@protonmail.com>
2016-08-21 00:13:41 +0200

    [WIP] Add support for new functions in configure.py

--- scripts/configure.py
+++ scripts/configure.py
@@ -5,6 +5,7 @@ import signal
 import readline
 import sys
 import getopt
+import os
 
 
 class Completer(object):
@@ -50,6 +51,7 @@ class Bool(Function):
             elif read_input.lower() == 'y' or read_input.lower() == 'n':
                 return variable_name, read_input[0]
 
+
     @staticmethod
     def __resolve_choice_text(arguments):
         default = "n" if not arguments else ''.join(arguments[0]).strip('[]')
@@ -61,7 +63,7 @@ class Option(Function):
     name = "option"
     @staticmethod
     def execute(arguments):
-        readline.set_completer(Completer(arguments[2:len(arguments) - 1]).complete)
+        readline.set_completer(Completer(arguments[2:-1]).complete)
         query = arguments[0]
         variable_name = arguments[1]
         if len(arguments) > 1:
@@ -91,6 +93,70 @@ class HashComment(Function):
         print('#')
         print('# ' + ' '.join(arguments))
         print('#')
+        return "", ""
+
+
+class Output(Function):
+    name = "output"
+    @staticmethod
+    def execute(arguments):
+        for generator in generator_list:
+            if generator.name == arguments[1]:
+                interpreter.generators.append(generator(open(arguments[0], "w")))
+                return "", ""
+        return "", ""
+
+
+class If(Function):
+    name = "if"
+    @staticmethod
+    def execute(arguments):
+        left = interpreter.read_variable(arguments[0])
+        condition = arguments[1]
+        if not left:
+            left = arguments[0]
+        right = interpreter.read_variable(arguments[2])
+        if not right:
+            right = arguments[2]
+        if condition == "==":
+            if left == right:
+                interpreter.condition_stack.append(True)
+            else:
+                interpreter.condition_stack.append(False)
+        return "", ""
+
+
+class Else(Function):
+    name = "else"
+    @staticmethod
+    def execute(arguments):
+        interpreter.condition_stack[-1] = not interpreter.condition_stack[-1]
+        return "", ""
+
+
+class EndIf(Function):
+    name = "endif"
+    @staticmethod
+    def execute(arguments):
+        if not len(interpreter.condition_stack):
+            raise IndexError
+        interpreter.condition_stack.pop()
+        return "", ""
+
+
+class Set(Function):
+    name = "set"
+    @staticmethod
+    def execute(arguments):
+        return arguments[0], arguments[1]
+
+
+class Die(Function):
+    name = "die"
+    @staticmethod
+    def execute(arguments):
+        print("Error: {}".format(" ".join(arguments)))
+        sys.exit(1)
         return "", ""
 
 
@@ -134,6 +200,11 @@ class CHeaderGenerator(Generator):
             self.output_file.write("#define {} {}\r\n".format(name, value))
 
 
+def error_handler(error_string, line, line_number):
+    print("Error at line {}: {}".format(line_number, error_string))
+    print("\t-> {}".format(line.strip("\r\n")))
+    sys.exit(1)
+
 
 class Variable:
 
@@ -145,32 +216,61 @@ class Variable:
         self.value = _value
 
 
+class VariableList:
 
-def error_handler(error_string, line, line_number):
-    print("Error at line {}: {}".format(line_number, error_string))
-    print("\t-> {}".format(line.strip("\r\n")))
-    sys.exit(1)
+    __variables = {}
+
+    def get_variable(self, name):
+        if name in self.__variables:
+            return self.__variables[name]
+        return ""
+
+
+    def set_variable(self, name, value):
+        self.__variables[name] = value
+
+
+    def unset_variable(self, name):
+        pass
+
+
+    def get(self):
+        values = []
+        names = []
+        for key, value in self.__variables.items():
+            names.append(key)
+            values.append(value)
+        return names, values
 
 
 class Interpreter:
 
-    list_of_variables = []
-    stack = []
-    builtins = [Bool(), Option(), HashComment(), Include()]
+    line = ""
+    variable_list = VariableList()
+    condition_stack = []
+    builtins = {"bool":Bool(), "option":Option(), "#":HashComment(), "if":If(), "else":Else(), "endif":EndIf(), "set":Set(), "include":Include(), "output":Output(), "die":Die()}
+    generators = []
+
 
     def __init__(self):
         pass
 
 
-    def __get_function(self, function_name):
-        for function in self.builtins:
-            if function.name == function_name:
-                return function
+    def __get_function(self, name):
+        if name in self.builtins:
+            return self.builtins[name]
         raise NotImplementedError
 
 
+    def read_variable(self, variable_name):
+        return self.variable_list.get_variable(variable_name)
+
+
     def parse_line(self, _line, line_number):
+
         for parsed in csv.reader([_line], delimiter=' ', quotechar='"'):
+
+            line = _line
 
             if len(parsed) == 0:
                 return
@@ -181,16 +281,31 @@ class Interpreter:
             function_name = parsed[0]
             arguments = parsed[1:len(parsed)]
 
+            if len(self.condition_stack):
+                if self.condition_stack[-1] == False:
+                    if not function_name == "endif" and not function_name == "else":
+                       return
+
             try:
                 function = self.__get_function(function_name)
                 variable_name, value = function.execute(arguments)
                 if variable_name and value:
-                    self.list_of_variables.append(Variable(variable_name, value))
+                    self.variable_list.set_variable(variable_name, value)
 
             except IndexError:
                 error_handler("Not enough arguments for: \'{}\'".format(function_name), _line, line_number)
             except NotImplementedError:
                 error_handler("No such keyword: \'{}\'".format(function_name), _line, line_number)
+
+
+    def generate_output(self):
+        print("\nConfiguration:")
+        names, values = self.variable_list.get()
+        for name, value in zip(names, values):
+            print("{} = {}".format(name, value))
+            for generator in self.generators:
+                generator.generate_define(name, value)
+        pass
 
 
 def parse_file(filename):
@@ -212,42 +327,24 @@ if __name__ == '__main__':
     interpreter = Interpreter()
     signal.signal(signal.SIGINT, signal_handler)
     readline.parse_and_bind('tab: complete')
-    generators = []
     generator_list = [CMakeGenerator, CHeaderGenerator]
     input_filename = ''
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:", ["ifile="])
     except getopt.GetoptError:
-        print('configure.py -i <inputfile> -o <outputfile>')
+        print('configure.py -i <inputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('configure.py -i <inputfile> -o <outputfile>')
+            print('configure.py -i <inputfile>')
             sys.exit(0)
-        elif opt in ("-o", "--ofile"):
-            output = str(arg).split(',')
-            for generator in generator_list:
-                if generator.name == str(output[0]):
-                    generators.append(generator(open(str(output[1]), 'w')))
-                    break
         elif opt in ("-i", "--ifile"):
             input_filename = arg
         else:
             print("Not recognized option: {}".format(arg))
             sys.exit(1)
 
-    if len(generators) == 0:
-        print("No output files!")
-        sys.exit(1)
-
     parse_file(input_filename)
-    print("\nConfiguration:")
-    for variable in interpreter.list_of_variables:
-        print("{} = {}".format(variable.name, variable.value))
-        for generator in generators:
-            generator.generate_define(variable.name, variable.value)
-    print("")
-
-
+    interpreter.generate_output()
 
