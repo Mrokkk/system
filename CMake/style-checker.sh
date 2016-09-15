aa71322b0d74901ffee4641116c2c06de896e69a
Maciek <mrokk@protonmail.com>
2016-09-15 21:08:37 +0200

    Update style checker

--- CMake/style-checker.sh
+++ CMake/style-checker.sh
@@ -2,21 +2,38 @@
 
 LANG=C
 
-if [ "$2" == "--f" ]; then
+ASTYLE="astyle --style=google \
+               --indent-switches \
+               --indent-col1-comments \
+               --min-conditional-indent=0 \
+               --max-instatement-indent=40 \
+               --pad-oper \
+               --pad-header \
+               --align-pointer=name \
+               --align-reference=name \
+               --remove-brackets \
+               --convert-tabs \
+               --mode=c \
+               --break-after-logical "
+
+TOTAL=0
+BAD=0
+
+if [ "$2" == "-f" ]; then
     CHECK_FILES=1
 fi
-
-ASTYLE="astyle --style=google --indent-switches --indent-col1-comments --pad-oper --align-pointer=name --align-reference=name --remove-brackets --convert-tabs --mode=c "
 
 cd $1
 
 for file in $(find . -name "*.[c|h]"); do
+    (( TOTAL=$TOTAL+1 ))
     if ! ${ASTYLE} < "${file}" | diff ${file} - > /dev/null; then
+        (( BAD=$BAD+1 ))
         echo -e "File $file \e[31mFAILED\e[0m"
         if [ $CHECK_FILES ]; then
-            echo -n "Show diff? "
+            echo -n "Show diff? [y/N] "
             read input
-            if [ $input == 'y' ]; then
+            if [ "$input" == "y" ]; then
                 ${ASTYLE} < "${file}" | diff --color=always ${file} -
             fi
         fi
@@ -24,4 +41,6 @@ for file in $(find . -name "*.[c|h]"); do
         echo -e "File $file \e[32mSUCCEED\e[0m"
     fi
 done
+
+echo "Bad formatted files: $BAD/$TOTAL"
 
