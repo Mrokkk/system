7b25eed45bbb9bc9d2126d4e4ce193145b29c2f5
Mrokkk <mrokk@protonmail.com>
2016-11-15 23:45:55 +0100

    Refactor sys_read

--- fs/read_write.c
+++ fs/read_write.c
@@ -18,16 +18,13 @@ int sys_write(int fd, const char *buffer, size_t size) {
 int sys_read(int fd, char *buffer, size_t n) {
 
     struct file *file;
-    int res;
 
     process_fd_get(process_current, fd, &file);
 
     if (!file) return -EBADF;
     if (!file->ops) return -ENODEV;
     if (!file->ops->read) return -ENODEV;
-    res = file->ops->read(file->inode, file, buffer, n);
-
-    return res;
+    return file->ops->read(file->inode, file, buffer, n);
 
 }
 
