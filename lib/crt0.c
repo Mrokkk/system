4166038cffd10863c37195832c94ae742affb6bc
Mrokkk <mrokk@protonmail.com>
2024-05-31 17:07:00 +0200

    lib/crt0.c: call global ctors and dtors

--- lib/crt0.c
+++ lib/crt0.c
@@ -1,8 +1,27 @@
+typedef void (*ctor_t)();
+
+extern ctor_t __init_array_start[], __init_array_end[];
+extern ctor_t __fini_array_start[], __fini_array_end[];
+extern ctor_t __preinit_array_start[], __preinit_array_end[];
+
+__attribute__((used)) void _call_global_ctors(void)
+{
+    for (ctor_t* ctor = __preinit_array_start; ctor != __preinit_array_end; (*ctor++)());
+    for (ctor_t* ctor = __init_array_start; ctor != __init_array_end; (*ctor++)());
+}
+
+__attribute__((used)) void _call_global_dtors(void)
+{
+    for (ctor_t* dtor = __fini_array_start; dtor != __fini_array_end; (*dtor++)());
+}
+
 __attribute__((naked,noreturn,used)) void _start(int, char**)
 {
     asm volatile(
         "call __libc_start_main;"
+        "call _call_global_ctors;"
         "call main;"
         "push %eax;"
+        "call _call_global_dtors;"
         "call exit;");
 }
