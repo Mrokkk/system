9afae86a73f376d6cc8a889bb003b01449d35dbd
Mrokkk <mrokk@protonmail.com>
2024-05-31 17:47:52 +0200

    stdlib: add atoi
    * add limits.h
    * add test for atoi

--- bin/test/main.c
+++ bin/test/main.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
@@ -255,6 +256,47 @@ TEST(bad_syscall)
     int ret = syscall(999, 0, 0);
     EXPECT_EQ(ret, -1);
     EXPECT_EQ(errno, ENOSYS);
+}
+
+TEST(atoi)
+{
+    char buf[128];
+    {
+        sprintf(buf, "154");
+        EXPECT_EQ(atoi(buf), 154);
+    }
+    {
+        sprintf(buf, "    4952tkskg ");
+        EXPECT_EQ(atoi(buf), 4952);
+    }
+    {
+        sprintf(buf, "abc");
+        EXPECT_EQ(atoi(buf), 0);
+    }
+    {
+        sprintf(buf, "z930");
+        EXPECT_EQ(atoi(buf), 0);
+    }
+    {
+        buf[0] = 0;
+        EXPECT_EQ(atoi(buf), 0);
+    }
+    {
+        sprintf(buf, "-1");
+        EXPECT_EQ(atoi(buf), -1);
+    }
+    {
+        sprintf(buf, "-23450");
+        EXPECT_EQ(atoi(buf), -23450);
+    }
+    {
+        sprintf(buf, "%d", INT32_MIN);
+        EXPECT_EQ(atoi(buf), INT32_MIN);
+    }
+    {
+        sprintf(buf, "40934950");
+        EXPECT_EQ(atoi(buf), 40934950);
+    }
 }
 
 TEST_SUITE_END();
--- lib/include/stdlib.h
+++ lib/include/stdlib.h
@@ -5,7 +5,7 @@
 void* malloc(size_t size);
 void* calloc(size_t nmemb, size_t size);
 void* realloc(void* ptr, size_t size);
-void free(void* ptr); // FIXME: non-standard free
+void free(void* ptr);
 
 [[noreturn]] void exit(int retcode);
 [[noreturn]] void abort(void);
@@ -13,5 +13,9 @@ void free(void* ptr); // FIXME: non-standard free
 char* getenv(const char* name);
 char* secure_getenv(const char* name);
 
+int atoi(const char* nptr);
+
 #define EXIT_SUCCESS    0
 #define EXIT_FAILURE    -1
+
+#define MB_CUR_MAX      1
