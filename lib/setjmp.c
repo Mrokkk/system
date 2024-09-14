b91fde861257e7d139921c7837aafaa7283c6dde
Mrokkk <mrokk@protonmail.com>
2024-09-15 00:57:45 +0200

    setjmp: fix bug caused by additional call in sig* functions

--- lib/setjmp.c
+++ lib/setjmp.c
@@ -2,50 +2,50 @@
 
 _Static_assert(sizeof(struct __jmp_buf) == 24);
 
+#define LONGJMP() \
+    asm volatile( \
+        "mov 4(%esp), %ecx;" \
+        "mov 0(%ecx), %ebx;" \
+        "mov 4(%ecx), %esi;" \
+        "mov 8(%ecx), %edi;" \
+        "mov 12(%ecx), %edx;" \
+        "mov 8(%esp), %eax;" \
+        "mov 16(%ecx), %ebp;" \
+        "mov 20(%ecx), %esp;" \
+        "jmp *%edx")
+
+#define SETJMP() \
+    asm volatile( \
+        "mov 4(%esp), %ecx;" \
+        "mov %ebx, 0(%ecx);" \
+        "mov %esi, 4(%ecx);" \
+        "mov %edi, 8(%ecx);" \
+        "mov (%esp), %eax;" \
+        "movl %eax, 12(%ecx);" \
+        "mov %ebp, 16(%ecx);" \
+        "mov %esp, 20(%ecx);" \
+        "xor %eax, %eax;" \
+        "ret")
+
 __attribute__((naked)) void LIBC(longjmp)(jmp_buf, int)
 {
-    asm volatile(
-        "mov 4(%esp), %ecx;"
-        "mov 0(%ecx), %ebx;"
-        "mov 4(%ecx), %esi;"
-        "mov 8(%ecx), %edi;"
-        "mov 12(%ecx), %edx;"
-        "mov 8(%esp), %eax;"
-        "mov 16(%ecx), %ebp;"
-        "mov 20(%ecx), %esp;"
-        "jmp *%edx");
+    LONGJMP();
 }
 
 __attribute__((naked)) int LIBC(setjmp)(jmp_buf)
 {
-    asm volatile(
-        "mov 4(%esp), %ecx;"
-        "mov %ebx, 0(%ecx);"
-        "mov %esi, 4(%ecx);"
-        "mov %edi, 8(%ecx);"
-        "mov (%esp), %eax;"
-        "movl %eax, 12(%ecx);"
-        "mov %ebp, 16(%ecx);"
-        "mov %esp, 20(%ecx);"
-        "xor %eax, %eax;"
-        "ret");
+    SETJMP();
 }
 
-// FIXME: implement those properly
-void LIBC(siglongjmp)(sigjmp_buf buf, int val)
+__attribute__((naked)) void LIBC(siglongjmp)(jmp_buf, int)
 {
-    return LIBC(longjmp)(buf, val);
+    LONGJMP();
 }
 
-DIAG_IGNORE("-Wanalyzer-use-of-uninitialized-value");
-
-int LIBC(sigsetjmp)(sigjmp_buf buf, int savesig)
+__attribute__((naked)) int LIBC(sigsetjmp)(jmp_buf, int)
 {
-    UNUSED(savesig);
-    return LIBC(setjmp)(buf);
+    SETJMP();
 }
-
-DIAG_RESTORE();
 
 LIBC_ALIAS(longjmp);
 LIBC_ALIAS(setjmp);
